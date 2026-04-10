c892c8561d7ceb98fa8ab6f26badb6fb5dd9a953
noa.la <noa.la@lycee-de-beauvoir.education>
2026-04-10 11:55:35 +0200

    JE VAIS TABASSER LE CONCOURS

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 from tkinter import *
+import serial
+import modbus
 vitesse_transmission = "9600"
-port_usb = "COM20"
+port = "COM4"
+requete_modbus = bytes([0x01,0x03,0x00,0x00, 0x00, 0x05, 0x85, 0xC9])
+reponse_modbus=""
+
+
+def envoyer_requete():
+    global port
+    global vitesse_transmission
+    global requete_modbus
+    global reponse_modbus
+    try:
+        reponse_modbus = modbus.envoyer_donnees(port, vitesse_transmission, requete_modbus)
+        trame_reponse_label.config(text=f"{reponse_modbus}")
+        if reponse_modbus[4] == 0x00:
+            statut_alarme.config(bg="green",text="RAS")
+        elif reponse_modbus[4] == 0x01:
+            statut_alarme.config(bg="yellow",text="Alarme")
+        elif reponse_modbus[4] == 0x02:
+            statut_alarme.config(bg="red",text="CRITIQUE")
+    except serial.serialutil.SerialException:
+        pass
 
 fenetre = Tk()
 
-fenetre.title("Capteur modbus rs485")
+fenetre.title("Interface IHM pour capteur modbus rs485")
 fenetre.geometry("600x500")
 
-titre = Label(fenetre, text="Interface IHM pour capteur modbus rs485",bg="yellow",pady=20)
-titre.pack(fill="x")
+#**************************************MESSAGE UTILISATEUR*************************************
 
-frame_configuration = LabelFrame(fenetre, text="Configuration",pady=10, padx=10)
+frame_message = LabelFrame(fenetre, borderwidth=5, relief="sunken",padx=5, pady=5,text="Avertissements utilisateur",bg="beige")
+frame_message.pack(expand=True,fill="x",padx=5,pady=5)
+
+#*************************CONFIGURATION************************************************
+frame_configuration = LabelFrame(fenetre, text="Configuration",pady=10, padx=10, borderwidth=5, relief="sunken")
 frame_configuration.pack(fill="x",padx=10,expand=True)
 
-statut_alarme = Label(frame_configuration,height=2,width=5,bg="green",text="RAS")
+statut_alarme = Label(frame_message,height=2,width=5,bg="green",text="RAS")
 statut_alarme.pack(side="left")
 
 BAUDRATE_label = Label(frame_configuration, text="Vitesse de transmission : ", padx=10)
@@ -26,26 +51,53 @@ BAUDRATE.pack(side="left")
 port_label = Label(frame_configuration,text="Port : ",padx=20)
 port_label.pack(side="left")
 
-port = Entry(frame_configuration,width=10)
-port.insert(0,port_usb)
-port.pack(side="left")
+port_entry = Entry(frame_configuration,width=10)
+port_entry.insert(0,port)
+port_entry.pack(side="left")
 
-frame_requete_modbus = LabelFrame(fenetre, text="Requête Modbus", pady=10, padx=10)
-frame_requete_modbus.pack(fill="x", padx=10,expand=True)
+#*************************REQUETE MODBUS************************************************
 
-requete_label = Label(frame_requete_modbus, text="Requête", padx=20)
+frame_requete = LabelFrame(fenetre, text="Requête Modbus", pady=10, padx=10, borderwidth=5, relief="sunken")
+frame_requete.pack(fill="x", padx=10,expand=True)
+
+requete_label = Label(frame_requete, text="Requête", padx=20)
 requete_label.pack(side="left")
 
-requete = Entry(frame_requete_modbus, width=50)
+requete = Entry(frame_requete, width=50)
 requete.pack(side="left")
+requete.insert(0,requete_modbus.hex())
 
-Envoyer = Button(frame_requete_modbus, text="Envoyer", command=envoyer_donnees)
+Envoyer = Button(frame_requete, text="Envoyer", command=envoyer_requete)
 Envoyer.pack(side="right")
 
-frame_reponse_modbus = LabelFrame(fenetre, text="Données reçues", pady=10, padx=10)
+#*************************REPONSE MODBUS************************************************
+
+frame_reponse_modbus = LabelFrame(fenetre, text="Données reçues", pady=10, padx=10, borderwidth=5, relief="sunken")
 frame_reponse_modbus.pack(fill="x", padx=10,expand=True)
 
 reponse_label = Label(frame_reponse_modbus, text="reponse", padx=20)
 reponse_label.pack(side="left")
+
+
+trame_reponse_label = Label(frame_reponse_modbus, text="trame", padx=20)
+trame_reponse_label.pack(side="left")
+
+'''sous_frame_temperature = LabelFrame(frame_reponse_modbus, text="Temperature", padx=10)
+sous_frame_temperature.pack(side="bottom")
+
+sous_frame_humidite = LabelFrame(frame_reponse_modbus, text="Humidite", padx=10)
+sous_frame_humidite.pack(side="bottom")
+
+sous_frame_pression = LabelFrame(frame_reponse_modbus, text="Pressure", padx=10)
+sous_frame_pression.pack(side="bottom")
+
+sous_frame_temperature_label = Label(sous_frame_temperature, text="temperature", padx=20)
+sous_frame_temperature_label.pack(side="left")
+
+sous_frame_humidite_label = Label(sous_frame_humidite, text="humidity", padx=20)
+sous_frame_humidite_label.pack(side="left")
+
+sous_frame_pression_label = Label(sous_frame_pression, text="pressure", padx=20)
+sous_frame_pression_label.pack(side="left")'''
 
 fenetre.mainloop()
--- modbus.py
+++ modbus.py
@@ -1,24 +1,12 @@
 import serial
 import time
-from main import port_usb, vitesse_transmission
-
-PORT = port_usb
-
-def envoyer_donnees():
 
 
-    requete = bytes([0x01,0x03,0x00,0x00, 0x00, 0x05, 0x85, 0xC9])
-
-    ser = serial.Serial(PORT, int(vitesse_transmission), timeout=1)
-
+def envoyer_donnees(port, vitesse_transmission, requete):
+    ser = serial.Serial(port, int(vitesse_transmission), timeout=1)
     ser.write(requete)
-
     time.sleep(0.1)
-
     reponse = ser.read(15)
-
-    print(reponse)
-
+    print(reponse.hex())
     ser.close()
-
-    return reponse
+    return reponse.hex()
